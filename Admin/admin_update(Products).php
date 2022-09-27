65fa997229f2bbce8731922ec95835ffaac9507b
pjaaaay <boruettpj@gmail.com>
2022-09-27 23:31:10 +0300

    new changes

--- Admin/admin_update(Products).php
+++ Admin/admin_update(Products).php
@@ -1,3 +1,80 @@
+<?php
+
+require("connections.php");
+
+$prod_id=$_GET['edit'];
+
+
+if (isset($_POST['update_product'])) {
+    
+
+
+
+
+$p_name=$_POST['product_name'];
+
+
+$p_des=$_POST['product_description'];
+
+
+$p_price=$_POST['product_price'];
+
+
+$p_quan=$_POST['available_quantity'];
+
+
+$sub_n =$_POST['subcategory_name'];
+
+
+$cat_n=$_POST['category_name'];
+
+
+$key=$_POST['product_keywords'];
+ 
+$p_image= $_FILES['product_image']['name'];
+$p_image_tmp_name = $_FILES['product_image']['tmp_name'];
+$file_type = 'uploaded_image/' .$p_image;
+	//print($file_type);
+
+
+
+
+$sql="UPDATE tbl_product SET product_name= '$p_name' ,product_description= '$p_des' ,product_image = '$p_image' ,available_quantity ='$p_quan' , subcategory_name = '$sub_n',category_name = '$cat_n',product_keywords= '$key' WHERE product_id= $prod_id";
+
+
+$query= mysqli_query($conn,$sql);
+
+
+if ($query){
+
+   $sql2="INSERT INTO tbl_productimages(product_image)
+   VALUES('$p_image')";
+ 
+
+
+   $upload= mysqli_query($conn,$sql2);
+   if($upload){
+       move_uploaded_file($p_image_tmp_name,$file_type);
+       header('location:admin_page(Products).php');
+   }
+
+   else{ 
+       echo"could not add the product";
+   }
+
+
+}
+
+    
+
+}
+
+
+else{
+    //http_respose_code(400);
+    echo"404 ERROR.Fill All Fields";
+};
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,11 +96,16 @@
 
 
 <div class="admin-product-form-container centered">
-   <form action="" method="post" enctype="multipart/form-data"><!-----specifies how form data should be encoded----->
+               <?php
+                $select = mysqli_query($conn,"SELECT * FROM tbl_product WHERE product_id=$prod_id");
+                while($row = mysqli_fetch_assoc($select)){
+                    
+                ?>
+   <form action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data"><!-----specifies how form data should be encoded----->
       <h3 class="title">update the product</h3>
-      <input type="text" class="box" name="product_name" value="" placeholder="enter the product name">
-      <input type="int" min="0" class="box" name="product_price" value="" placeholder="enter the product price">
-      <input type="text" placeholder="enter product description" name="product_description" value="" class="box">
+      <input type="text" class="box" name="product_name" value="<?php echo $row['product_name'];?>" placeholder="enter the product name">
+      <input type="int" min="0" class="box" name="product_price" value="<?php echo $row['unit_price'];?>" placeholder="enter the product price">
+      <input type="text" placeholder="enter product description" name="product_description" value="<?php echo $row['product_description'];?>" class="box">
       <select type="text" class="box" name="subcategory_name">
             <option value="" disabled selected hidden>enter subcategory name</option>
             <option value="Formal">Formal</option>
@@ -40,12 +122,16 @@
             <option value="Children">Children</option>
             <option value="Pets">Pets</option>
       </select><br>
-      <input type="int" placeholder="enter available quantity" name="available_quantity" value="" class="box">
-      <input type="text" placeholder="enter product keywords" name="product_keywords" value="" class="box">
-      <input type="file" class="box" name="product_image"  accept="image/png, image/jpeg, image/jpg, image/webp" value="">
+      <input type="int" placeholder="enter available quantity" name="available_quantity" value="<?php echo $row['available_quantity'];?>" class="box">
+      <input type="text" placeholder="enter product keywords" name="product_keywords" value="<?php echo $row['product_keywords'];?>" class="box">
+      <input type="file" class="box" name="product_image"  accept="image/png, image/jpeg, image/jpg, image/webp" value="<?php echo $row['product_image'];?>">
       <input type="submit" value="update product" name="update_product" class="btn">
       <a href="admin_page(Products).php" class="btn">go back!</a>
    </form> 
+
+   
+ <?php
+ };?>
 </div>
 
 </div>
---
+++ Admin/productaddition.php
@@ -0,0 +1,73 @@
+<?php
+require("connections.php");
+
+
+if (isset($_POST['add_product'])) {
+    
+
+
+$p_name=$_POST['product_name'];
+
+
+$p_des=$_POST['product_description'];
+
+
+$p_price=$_POST['product_price'];
+
+
+$p_quan=$_POST['available_quantity'];
+
+
+$sub_n =$_POST['subcategory_name'];
+
+
+$cat_n=$_POST['category_name'];
+
+
+$key=$_POST['product_keywords'];
+
+ 
+$p_image= $_FILES['product_image']['name'];
+$p_image_tmp_name = $_FILES['product_image']['tmp_name'];
+$file_type = 'uploaded_image/' .$p_image;
+	//print($file_type);
+
+
+
+
+$sql1="INSERT INTO tbl_product(product_name,product_description,product_image,unit_price,available_quantity,subcategory_name,category_name,product_keywords)
+VALUES('$p_name','$p_des','$p_image','$p_price','$p_quan','$sub_n', '$cat_n', '$key')";
+
+$query= mysqli_query($conn,$sql1);
+
+if ($query){
+
+    $sql2="INSERT INTO tbl_productimages(product_image)
+    VALUES('$p_image')";
+  
+
+
+    $upload= mysqli_query($conn,$sql2);
+    if($upload){
+        move_uploaded_file($p_image_tmp_name,$file_type);
+        echo"New product successfully added";
+    }
+
+    else{ 
+        echo"could not add the product";
+    }
+
+
+    
+
+}}
+
+
+else{
+    //http_respose_code(400);
+    echo"404 ERROR.Fill All Fields";
+}
+
+
+
+?>
