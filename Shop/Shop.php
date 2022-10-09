7e5fa0acd7850dee299a11d31dfc8ee154d7533f
MikeMwai <mwai.michael@strathmore.edu>
2022-10-09 17:58:19 +0300

    Shop

--- Shop/Shop.php
+++ Shop/Shop.php
@@ -47,12 +47,12 @@
             	<li><a href="#"> COMING SOON... </a></li>
             </ul>
 			</li>
-			<li><b> <a href="#"><i class="fa fa-user"></i> ACCOUNT </a></b>
-				<ul>
-	<li><a href="#"> LogIn </a></li>
-	<li><a href="#"> Register/Sign Up </a></li>
-   </ul>
-   	</li>
+	 <li><b> <a href="../Account/Account.php"><i class="fa fa-user"></i> ACCOUNT </a></b>
+        <ul>
+	       <li><a href="../Vendor/productpage.php"> Vendor </a></li>
+	       <li><a href="../Vendor/Cart.php"> Cart </a></li>
+        </ul>
+	 </li>
 	<li id="contact"><b> <a href="#"> CONTACT US  </a></b></li>
 </ul>
 </header>
