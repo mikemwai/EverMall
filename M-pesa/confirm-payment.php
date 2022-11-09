bd5d0376072dbf08d0fdabe75799ca4c2a7cd4bd
J-Nyarangi <113386253+J-Nyarangi@users.noreply.github.com>
2022-11-09 07:43:58 +0300

    Update confirm-payment.php

--- M-pesa/confirm-payment.php
+++ M-pesa/confirm-payment.php
@@ -1,4 +1,10 @@
  <!--<h1>THANK YOU FOR SHOPPING</h1>--->
  <?php
- header('location:../Vendor/Cart.php'); 
+
+ echo "
+      <div class='order-message-container'>
+      <div class='message-container'>
+         <h3>Thank you for shopping! See you next time!</h3>";
+
+ header( "refresh:5;url=../Vendor/Cart.php" );
  ?>
